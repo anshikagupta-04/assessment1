24ceba6f186897239e3b2c55028432805c67e2b9
anshikagupta-04 <pg8998211@gmail.com>
2024-02-17 15:06:55 +0530

    1

---
+++ requirement4/readFile.js
@@ -0,0 +1,29 @@
+const fs = import('fs').promises;
+
+process.on('exit', (code) => {
+  console.log(`'exit' event called with code: ${code}`);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log("'unhandledRejection' event called");
+  throw reason;
+});
+
+const readFileSyncPromise = async (filename) => {
+  try {
+    const data = await fs.readFile(filename);
+    return data.toString();
+  } catch (error) {
+    console.error('Error reading file:', error.message);
+    throw error;
+  }
+};
+
+readFileSyncPromise('pom.txt')
+  .then((content) => {
+    console.log('File content:', content);
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+    throw new Error('Custom error from catch handler');
+  });
